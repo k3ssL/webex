d66b09b287aeb07e93d99296de83c86a01fe3bc6
Aleksey <viqwer3@yandex.ru>
2026-06-14 00:09:09 +0500

    fixes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -313,7 +313,7 @@ const About = () => {
                             Присоединяйтесь к сообществу ценителей надежности и безопасности
                         </p>
                         <div className={styles.cta_buttons}>
-                            <Link to={'/'} className={styles.cta_primary}>
+                            <Link to={'/catalog'} className={styles.cta_primary}>
                                 Начать покупки
                                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                                     <path d="M5 12h14M12 5l7 7-7 7"/>
